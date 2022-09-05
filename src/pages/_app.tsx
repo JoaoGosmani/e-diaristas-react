7807a22f79ac358f51595da3f317492d84726a71
Joao Gosmani <joaogosmani@gmail.com>
2022-09-05 11:23:53 -0300

    Criando o tema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '@styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "@mui/material";
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import theme from "ui/themes/theme";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-css-tags */
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
@@ -11,6 +12,20 @@ export default class MyDocument extends Document {
                 <link
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link 
+                  rel="preconnect" 
+                  href="https://fonts.gstatic.com" 
+                  crossOrigin="" 
+                />
+                <link 
+                  href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" 
+                  rel="stylesheet"
+                />
+                <link 
+                  href="/fonts/tw-icons/css/treinaweb-icons.css" 
+                  rel="stylesheet" 
                 />
                 {(this.props as any).emotionStyleTags}
                 </Head>
