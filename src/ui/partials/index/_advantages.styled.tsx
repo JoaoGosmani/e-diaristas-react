9ef945141f0c4ea9ce0955c793d085293a82f469
Joao Gosmani <joaogosmani@gmail.com>
2022-09-08 13:24:32 -0300

    Componente de Título da tela de Vantagens

--- src/ui/partials/index/_advantages.styled.tsx
+++ src/ui/partials/index/_advantages.styled.tsx
@@ -8,3 +8,32 @@ export const GradientBackground = styled("section")`
 
     color: ${({ theme }) => theme.palette.primary.contrastText};
 `;
+
+export const SectionTitle = styled("h2")`
+    position: relative;
+    font-weight: normal;
+    font-size: ${({ theme }) => theme.typography.h6.fontSize};
+    margin: 0;
+    padding: ${({ theme }) => theme.spacing(7, 0, 4)};
+    text-align: center;
+
+    &::after {
+        content: "";
+        position: absolute;
+        width: 44px;
+        height: 2px;
+        background-color: currentColor;
+        left: 50%;
+        bottom: ${({ theme }) => theme.spacing(2)};
+        transform: translateX(-50%);
+    }
+
+    ${({ theme }) => theme.breakpoints.up("md")} {
+        font-size: ${({ theme }) => theme.typography.h5.fontSize};
+        padding: ${({ theme }) => theme.spacing(15, 0, 13)};
+
+        &::after {
+            bottom: ${({ theme }) => theme.spacing(10)};
+        }
+    }
+`;
--- src/ui/partials/index/_advantages.tsx
+++ src/ui/partials/index/_advantages.tsx
@@ -1,7 +1,14 @@
-import { GradientBackground } from "./_advantages.styled";
+import { Container } from "@mui/material";
+import { GradientBackground, SectionTitle } from "./_advantages.styled";
 
 const Advantages = () => {
-    return <GradientBackground></GradientBackground>
+    return (
+        <GradientBackground>
+            <Container>
+                <SectionTitle>Por que usar o E-diaristas?</SectionTitle>
+            </Container>
+        </GradientBackground>
+    );
 };
 
 export default Advantages;
