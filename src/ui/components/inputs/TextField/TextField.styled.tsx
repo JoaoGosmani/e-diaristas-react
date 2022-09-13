48d0cb1ffc464bcadc7d488d8114009c4a2c9f86
Joao Gosmani <joaogosmani@gmail.com>
2022-09-13 09:22:58 -0300

    Componente TextField

---
+++ src/ui/components/inputs/TextField/TextField.styled.tsx
@@ -0,0 +1,13 @@
+import { styled } from "@mui/material/styles";
+import { TextField, TextFieldProps } from "@mui/material";
+// import { TextFieldProps } from "./TextField";
+
+export const TextFieldStyled = styled(TextField)<TextFieldProps>`
+   .MuiInputBase-root {
+    background-color: ${({ theme }) => theme.palette.grey[50]};
+   }
+
+   .MuiOutlinedInput-notchedOutline {
+    border-color: ${({ theme }) => theme.palette.grey[100]};
+   }
+`; 
---
+++ src/ui/components/inputs/TextField/TextField.tsx
@@ -0,0 +1,5 @@
+import React, { PropsWithChildren } from "react";
+// import {} from "@mui/material",
+import { TextFieldStyled } from "./TextField.styled";
+
+export default TextFieldStyled;
