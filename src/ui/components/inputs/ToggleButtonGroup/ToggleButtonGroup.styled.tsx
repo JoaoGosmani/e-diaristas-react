4540576040305f51a38deabc252f1cc13bb63dec
Joao Gosmani <joaogosmani@gmail.com>
2022-09-16 16:21:58 -0300

    Componente de Grupo de Botões

---
+++ src/ui/components/inputs/ToggleButtonGroup/ToggleButtonGroup.styled.tsx
@@ -0,0 +1,42 @@
+import { styled } from "@mui/material/styles";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+// import { ToggleButtonGroupProps } from "./ToggleButtonGroup";
+
+export const ToggleButtonGroupStyled = styled(ToggleButtonGroup)`
+  &.MuiToggleButtonGroup-root {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
+    gap: ${({ theme }) => theme.spacing(2)};
+  }
+`; 
+
+export const ToggleButtonStyled = styled(ToggleButton)`
+  &.MuiToggleButton-root.MuiToggleButtonGroup-groupedHorizontal {
+    border-radius: 3px;
+    border: 2px solid ${({ theme }) => theme.palette.grey[100]};
+    background-color: ${({ theme }) => theme.palette.grey[50]};
+    text-transform: none;
+
+    &.Mui-selected {
+      background-color: ${({ theme }) => theme.palette.secondary.main};
+      color: white;
+      border: 2px solid ${({ theme }) => theme.palette.secondary.dark};
+      &:hover {
+        background-color: ${({ theme }) => theme.palette.secondary.dark};
+      }
+    }
+
+    &.MuiToggleButton-root {
+      display: grid;
+
+      ${({ theme }) => theme.breakpoints.up("md")} {
+        gap: ${({ theme }) => theme.spacing()};
+        grid-auto-flow: column;
+      }
+    }
+
+    i {
+      font-size: 25px;
+    }
+  }
+`; 
---
+++ src/ui/components/inputs/ToggleButtonGroup/ToggleButtonGroup.tsx
@@ -0,0 +1,10 @@
+import React, { PropsWithChildren } from "react";
+// import {} from "@mui/material";
+import { ToggleButtonGroupStyled, ToggleButtonStyled } from "./ToggleButtonGroup.styled";
+
+export interface ToggleButtonGroupProps {}
+
+const ToggleButtonGroup = ToggleButtonGroupStyled;
+export const ToggleButton = ToggleButtonStyled;
+
+export default ToggleButtonGroup;
