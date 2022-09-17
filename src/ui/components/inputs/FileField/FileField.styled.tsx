1336f12e08a13d4fdea5ea60e343fd38c9c1c264
Joao Gosmani <joaogosmani@gmail.com>
2022-09-17 18:07:56 -0300

    Componente FileField

---
+++ src/ui/components/inputs/FileField/FileField.styled.tsx
@@ -0,0 +1,17 @@
+import { styled } from "@mui/material/styles";
+// import { } from "@mui/material";
+// import { FileFieldProps } from "./FileField";
+
+export const FileContainer = styled("div")`
+  position: relative;
+  .MuiTextField-root:last-of-type {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+  }
+`; 
+
+export const UploadIcon = styled("i")`
+  color: ${({ theme }) => theme.palette.text.secondary};
+`;
---
+++ src/ui/components/inputs/FileField/FileField.tsx
@@ -0,0 +1,45 @@
+import React, { ChangeEvent, PropsWithChildren, useState } from "react";
+import { TextFieldProps } from "@mui/material";
+import { FileContainer, UploadIcon } from "./FileField.styled";
+import TextField from "ui/components/inputs/TextField/TextField";
+
+export interface FileFieldProps extends Omit<TextFieldProps, "onChange"> {
+  onChange: (files: FileList) => void
+}
+
+const FileField:React.FC<PropsWithChildren<FileFieldProps>> = ({
+  onChange,
+  ...props
+}) => {
+  const [filePath, setFilePath] = useState("");
+
+  function handleFileChange(event: ChangeEvent) {
+    const target = event.target as HTMLInputElement,
+      files = target.files;
+
+      if (files !== null && files.length) {
+        setFilePath(files[0]?.name ?? "");
+        onChange(files);
+      } 
+  }
+
+    return (
+        <FileContainer>
+          <TextField 
+            label={"Selecione um arquivo"} 
+            value={filePath} 
+            InputProps={{ endAdornment: <UploadIcon className="twf-upload"/> }}
+            {...props}
+            fullWidth
+          />
+          <TextField 
+            type={"file"} 
+            {...props} 
+            fullWidth 
+            onChange={handleFileChange}
+          />
+        </FileContainer>
+    );
+};
+
+export default FileField;
