17102d0627642070030260bb985ae45f7d670a17
Joao Gosmani <joaogosmani@gmail.com>
2022-09-17 22:42:26 -0300

    Componente PasswordStrength

---
+++ src/ui/components/feedback/PasswordStrength/PasswordStrength.styled.tsx
@@ -0,0 +1,32 @@
+import { styled } from "@mui/material/styles";
+import { LinearProgress, Theme } from "@mui/material";
+// import { PasswordStrengthProps } from "./PasswordStrength";
+
+export const PasswordStrengthLabel = styled("span", {
+  shouldForwardProp: (prop) => prop !== "value",
+})<{ value: number }>`
+  font-weight: bold;
+  color: ${({ theme, value }) => handleBarColor(theme, value)};
+`; 
+
+export const PasswordStrengthBar = styled(LinearProgress)`
+  margin-top: ${({ theme }) => theme.spacing()};
+  &.MuiLinearProgress-root {
+    background-color: ${({ theme }) => theme.palette.grey[200]};
+  }
+
+  .MuiLinearProgress-bar {
+    background-color: ${({ theme, value }) => handleBarColor(theme, value)};
+  }
+`; 
+
+function handleBarColor(theme: Theme, value = 0) {
+  if (value <= 25) {
+    return theme.palette.error.main;
+  }
+  if (value > 25 && value <= 50) {
+    return theme.palette.warning.main;
+  }
+  
+  return theme.palette.success.main;
+}
---
+++ src/ui/components/feedback/PasswordStrength/PasswordStrength.tsx
@@ -0,0 +1,34 @@
+import React, { PropsWithChildren } from "react";
+import { Typography } from "@mui/material";
+import { passwordStrength } from "check-password-strength";
+import { 
+  PasswordStrengthLabel, 
+  PasswordStrengthBar,
+} from "./PasswordStrength.styled";
+
+export interface PasswordStrengthProps {
+  password: string;
+}
+
+const PasswordStrength:React.FC<PropsWithChildren<PasswordStrengthProps>> = ({
+  password,
+}) => {
+  const strength = password ? passwordStrength(password).id : 0,
+    strengthValue = ((strength + 1) / 4) * 100;
+  return (
+      <div style={{ gridArea: "password-strength" }}>
+        <Typography variant="body2" component={"span"} color={"textSecondary"}>
+          Nível da senha:&nbsp;
+          <PasswordStrengthLabel value={strengthValue}>
+            {strength === 0 && "FRACA"}
+            {strength === 1 && "MÉDIA"}
+            {strength === 2 && "FORTE"}
+            {strength === 3 && "FORTE"}
+          </PasswordStrengthLabel>
+        </Typography>
+        <PasswordStrengthBar variant="determinate" value={strengthValue} />
+      </div>
+  );
+};
+
+export default PasswordStrength;
