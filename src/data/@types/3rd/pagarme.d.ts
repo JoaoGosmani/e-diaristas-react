f8355eb28907f48e77b3680c31b82ac74b465738
Joao Gosmani <joaogosmani@gmail.com>
2022-09-26 15:04:07 -0300

    Implementando o Pagarme

---
+++ src/data/@types/3rd/pagarme.d.ts
@@ -0,0 +1,24 @@
+declare module "pagarme" {
+    export default Pagarme;
+
+    export interface CardInterface {
+        card_number: string;
+        card_holder_name: string;
+        card_expiration_date: string;
+        card_cvv: string;
+    }
+
+    export interface CardValidateInterface {
+        card_number: boolean;
+        card_holder_name: boolean;
+        card_expiration_date: boolean;
+        card_cvv: boolean;
+        band: string;
+    }
+}
+
+declare const Pagarme: PagarmeInterface;
+
+interface PagarmeInterface {
+    validate: ({ card: CardInterface }) => { card: CardValidateInterface };
+}
