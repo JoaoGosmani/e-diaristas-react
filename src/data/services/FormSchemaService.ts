d19ec03222fec2df7781bdfa9bd66bb071fc16e7
Joao Gosmani <joaogosmani@gmail.com>
2022-09-26 15:25:08 -0300

    Validando dados do cartão com o Yup

--- src/data/services/FormSchemaService.ts
+++ src/data/services/FormSchemaService.ts
@@ -1,10 +1,13 @@
 import * as yup from "yup";
 import { ValidationService } from "./ValidationService";
 import { DateService } from "./DateService";
+import { PaymentService } from "./PaymentService";
 
 export const FormSchemaService = {
     newContact() {
-        return yup.object().shape({
+        return yup
+            .object()
+            .shape({
             usuario: yup.object().shape({
                 email: yup.string().email("E-mail inválido"),
                 password: yup.string().min(5, "Senha muito curta"),
@@ -13,10 +16,13 @@ export const FormSchemaService = {
                     .min(5, "Senha muito curta")
                     .oneOf([yup.ref("password"), null], "As senhas não estão iguais"),
             }),
-        });
+            })
+            .defined();
     },
     userData() {
-        return yup.object().shape({
+        return yup
+            .object()
+            .shape({
             usuario: yup.object().shape({
                 nome_completo: yup.string().min(3, "Digite seu nome completo"),
                 nascimento: yup
@@ -30,6 +36,50 @@ export const FormSchemaService = {
                     .string()
                     .test("telefone", "Telefone inválido", ValidationService.telefone),
             }),
-        });
+            })
+            .defined();
+    },
+    payment() {
+        return yup
+            .object()
+            .shape({
+            pagamento: yup.object().shape({
+                numero_cartao: yup.string().test(
+                    "card_number", 
+                    "Número de cartão inválido", 
+                    (value) => 
+                        PaymentService.validate({
+                            card_number: value as string,
+                            card_holder_name: "",
+                            card_cvv: "",
+                            card_expiration_date: "",
+                        }).card_number
+                ),
+                nome_cartao: yup.string(),
+                validade: yup.string().test(
+                    "card_expiration_date", 
+                    "Data de validade inválida", 
+                    (value) => 
+                        PaymentService.validate({
+                            card_number: "",
+                            card_holder_name: "",
+                            card_cvv: "",
+                            card_expiration_date: value as string,
+                        }).card_expiration_date
+                ),
+                codigo: yup.string().test(
+                    "card_cvv", 
+                    "Código de validação inválido", 
+                    (value) => 
+                        PaymentService.validate({
+                            card_number: "",
+                            card_holder_name: "",
+                            card_cvv: value as string,
+                            card_expiration_date: "",
+                        }).card_cvv
+                ),
+            }),
+            })
+            .defined();
     },
 };
