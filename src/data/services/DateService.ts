58cd241f02f7079eb52be902f6d8bf148330c77f
Joao Gosmani <joaogosmani@gmail.com>
2022-09-26 14:35:19 -0300

    Validação do formulário de cadastro

---
+++ src/data/services/DateService.ts
@@ -0,0 +1,22 @@
+export const DateService = {
+    transformDate(value: any, originalValue: any): any {
+        if (typeof originalValue === "string") {
+            const [dia, mes, ano] = originalValue.split("/");
+            if (+mes < 1 || +mes > 12) {
+                return new Date("");
+            }
+            return new Date(+ano, +mes - 1, +dia);
+        }
+        return value;
+    },
+    minAdultBirthday(): Date {
+        const data = new Date();
+        data.setFullYear(data.getFullYear() - 18);
+        return data;
+    },
+    maxAdultBirthday(): Date {
+        const data = new Date();
+        data.setFullYear(data.getFullYear() - 100);
+        return data;
+    },
+};
--- src/data/services/FormSchemaService.ts
+++ src/data/services/FormSchemaService.ts
@@ -1,4 +1,6 @@
 import * as yup from "yup";
+import { ValidationService } from "./ValidationService";
+import { DateService } from "./DateService";
 
 export const FormSchemaService = {
     newContact() {
@@ -10,6 +12,23 @@ export const FormSchemaService = {
                     .string()
                     .min(5, "Senha muito curta")
                     .oneOf([yup.ref("password"), null], "As senhas não estão iguais"),
+            }),
+        });
+    },
+    userData() {
+        return yup.object().shape({
+            usuario: yup.object().shape({
+                nome_completo: yup.string().min(3, "Digite seu nome completo"),
+                nascimento: yup
+                    .date()
+                    .transform(DateService.transformDate)
+                    .min(DateService.maxAdultBirthday, "Digite uma data válida")
+                    .max(DateService.minAdultBirthday, "Proibido menores de idade")
+                    .typeError("Digite uma data válida"),
+                cpf: yup.string().test("cpf", "CPF inválido", ValidationService.cpf),
+                telefone: yup
+                    .string()
+                    .test("telefone", "Telefone inválido", ValidationService.telefone),
             }),
         });
     },
