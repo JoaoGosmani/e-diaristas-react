506b912a6310a375ccfb0bc55c64ed331a03fa16
Joao Gosmani <joaogosmani@gmail.com>
2022-09-27 12:08:01 -0300

    Listando cidades a partir de um estado específico

--- src/data/hooks/components/inputs/UserForm/forms/useAddressForm.ts
+++ src/data/hooks/components/inputs/UserForm/forms/useAddressForm.ts
@@ -1,4 +1,7 @@
 import { FormValues } from "data/@types/forms/FormValues";
+import useCities from "data/hooks/useCities";
+import { LocationService } from "data/services/LocationService";
+import { useMemo } from "react";
 import { useFormContext } from "react-hook-form";
 
 export default function useAddressForm() {
@@ -7,5 +10,16 @@ export default function useAddressForm() {
         control,
         watch,
         formState: { errors },
-    } = useFormContext<FormValues>();
+    } = useFormContext<FormValues>(),
+    [addressState, addressCity, addressCep] = watch([
+        "endereco.estado", 
+        "endereco.cidade", 
+        "endereco.cep",
+    ]),
+    estados = LocationService.estados(),
+    listaCidades = useCities(addressState),
+    opcoesCidades = useMemo(
+        () => listaCidades.map((item) => item.cidade), 
+        [listaCidades]
+    );
 } 
---
+++ src/data/hooks/useCities.ts
@@ -0,0 +1,16 @@
+import { CidadeInterface } from "data/@types/EnderecoInterface";
+import { LocationService } from "data/services/LocationService";
+import { useEffect, useState } from "react";
+
+export default function useCities(estado: string): CidadeInterface[] {
+    const [listaCidades, setListaCidades] = useState<CidadeInterface[]>([]);
+    useEffect(() => {
+        if (estado) {
+            setListaCidades([]);
+            LocationService.cidades(estado).then((listaCidades) => {
+                listaCidades && setListaCidades(listaCidades);
+            });
+        }
+    }, [estado]);
+    return listaCidades;
+}
