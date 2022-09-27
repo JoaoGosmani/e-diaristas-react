e98976e830452a7718dcb502e271d17158ae11dc
Joao Gosmani <joaogosmani@gmail.com>
2022-09-27 14:39:15 -0300

    Finalizando a lógica do formulário de endereços

--- src/data/hooks/components/inputs/UserForm/forms/useAddressForm.ts
+++ src/data/hooks/components/inputs/UserForm/forms/useAddressForm.ts
@@ -1,7 +1,7 @@
 import { FormValues } from "data/@types/forms/FormValues";
 import useCities from "data/hooks/useCities";
 import { LocationService } from "data/services/LocationService";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useFormContext } from "react-hook-form";
 
 export default function useAddressForm() {
@@ -9,6 +9,7 @@ export default function useAddressForm() {
         register,
         control,
         watch,
+        setValue,
         formState: { errors },
     } = useFormContext<FormValues>(),
     [addressState, addressCity, addressCep] = watch([
@@ -22,4 +23,46 @@ export default function useAddressForm() {
         () => listaCidades.map((item) => item.cidade), 
         [listaCidades]
     );
+
+    useEffect(() => {
+        register("endereco.codigo_ibge")
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        if (addressCity) {
+            const cidade = listaCidades.find((item) => item.cidade === addressCity);
+            if (cidade) {
+                setValue("endereco.codigo_ibge", cidade.codigo_ibge.toString());
+            }
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [addressCity]);
+
+    useEffect(() => {
+        const cep = (addressCep ?? "").replaceAll("_", "");
+        if (cep.length === 10) {
+            LocationService.cep(cep).then((newAddress) => {
+                if (newAddress) {
+                    newAddress.uf && setValue("endereco.estado", newAddress.uf);
+                    newAddress.localidade && setValue("endereco.cidade", newAddress.localidade);
+                    newAddress.ibge && setValue("endereco.codigo_ibge", newAddress.ibge.toString());
+                    newAddress.bairro && setValue("endereco.bairro", newAddress.bairro);
+                    newAddress.logradouro && setValue("endereco.logradouro", newAddress.logradouro);
+                }
+            });
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [addressCep]);
+
+    return {
+        addressCep,
+        addressCity,
+        addressState,
+        register,
+        estados,
+        opcoesCidades,
+        control,
+        errors,
+      };
 } 
---
+++ src/ui/components/inputs/UserForm/forms/AddressForm.tsx
@@ -0,0 +1,8 @@
+import useAddressForm from "data/hooks/components/inputs/UserForm/forms/useAddressForm"
+
+export const AddressForm = () => {
+    const { control, errors, estados, opcoesCidades, addressState, register } = 
+        useAddressForm();
+
+    return <div></div>
+};
