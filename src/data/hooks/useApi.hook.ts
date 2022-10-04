a8bff0cf9782254c375df202836f4b3f8791d517
Joao Gosmani <joaogosmani@gmail.com>
2022-10-04 14:19:30 -0300

    Melhorando a utilização do HATEOAS com Axios e SWR

--- src/data/hooks/useApi.hook.ts
+++ src/data/hooks/useApi.hook.ts
@@ -1,6 +1,8 @@
-import { AxiosRequestConfig } from "axios";
-import { ApiService } from "data/services/ApiService";
-import useSwr from "swr";
+import { AxiosError, AxiosRequestConfig } from "axios";
+import { ApiLinksInterface } from "data/@types/ApiLinksInterface";
+import { ApiService, ApiServiceHateoas } from "data/services/ApiService";
+import { useEffect, useCallback } from "react";
+import useSwr, { mutate } from "swr";
 
 export default function useApi<OutputType>(
     endPoint: string | null,
@@ -11,6 +13,32 @@ export default function useApi<OutputType>(
 
         return response.data;
     });
+
+    return { data, error };
+}
+
+export function useApiHateoas<OutputType, Error = unknown>(
+    links: ApiLinksInterface[] = [],
+    nome: string | null,
+    config?: AxiosRequestConfig   
+): { data: OutputType | undefined; error: AxiosError<Error> | undefined } {
+    const makeRequest = useCallback(() => {
+        return new Promise<OutputType>((resolve) => {
+            ApiServiceHateoas(links, nome ?? "", async (request) => {
+                const response = await request<OutputType>(config);
+                resolve(response.data);
+            });
+        });
+    }, [links, nome, config]);
+
+    const { data, error } = useSwr<OutputType, AxiosError<Error>>(
+        nome,
+        makeRequest
+    );
+
+    useEffect(() => {
+        mutate(nome, makeRequest);
+    }, [links, nome, makeRequest]);
 
     return { data, error };
 }
