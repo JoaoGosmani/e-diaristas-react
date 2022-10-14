7cd023d6dcb39a354e15a7a20936a457dc2c27a3
Joao Gosmani <joaogosmani@gmail.com>
2022-10-14 16:53:35 -0300

    Informações sobre o serviço do diarista ao usuário

--- src/pages/cadastro/diarista.tsx
+++ src/pages/cadastro/diarista.tsx
@@ -6,6 +6,9 @@ import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import Link from 'ui/components/navigation/Link/Link';
 import useCadastroDiarista from 'data/hooks/pages/cadastro/useCadastroDiarista.page';
 import { BrowserService } from 'data/services/BrowserService';
+import { PageFormContainer, UserFormContainer } from 'ui/components/inputs/UserForm/UserForm';
+import SideInformation from 'ui/components/data-display/SideInformation/SideInformation';
+import useIsMobile from 'data/hooks/useIsMobile';
 
 // import { Component } from "@styles/pages/cadastro/diarista.styled";
 
@@ -18,7 +21,8 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const Diarista: React.FC = () => {
-  const { breadcrumbItems, step, setStep } = useCadastroDiarista();
+  const { breadcrumbItems, step, setStep } = useCadastroDiarista(),
+    isMobile = useIsMobile();
 
   useEffect(() => {
     BrowserService.scrollToTop();
@@ -53,6 +57,38 @@ const Diarista: React.FC = () => {
         }
       />
       )}
+
+      <UserFormContainer>
+        <PageFormContainer>
+          <div></div>
+          {!isMobile && (
+            <SideInformation 
+              title="Como funciona?"
+              items={[
+                {
+                  title: "1 - Cadastro",
+                  description: [
+                    "Você faz o cadastro e escolhe as cidades atendidas",
+                  ],
+                },
+                {
+                  title: "2 - Receba Propostas",
+                  description: [
+                    "Você receberá avisos por E-mail sobre novos serviços nas cidades atendidas",
+                  ],
+                },
+                {
+                  title: "3 - Diária Agendada",
+                  description: [
+                    "Se o seu perfil for escolhido pelo cliente, você receberá a confirmação do agendamento",
+                  ],
+                },
+              ]}
+            />
+          )} 
+          
+        </PageFormContainer>
+      </UserFormContainer>
     </div>
   );
 };
