35060cbd4268a3d504ebf7a7d5148dd85f2cf197
Joao Gosmani <joaogosmani@gmail.com>
2022-10-18 10:35:29 -0300

    Modal para oportunidade selecionada

--- src/pages/oportunidades.tsx
+++ src/pages/oportunidades.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { GetStaticProps } from 'next';
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, Divider, Typography } from '@mui/material';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import useOportunidades from 'data/hooks/pages/useOportunidades.page';
 import DataList from 'ui/components/data-display/DataList/DataList';
 import { TextFormatService } from 'data/services/TextFormatService';
 import Table, { TableCell, TablePagination, TableRow } from 'ui/components/data-display/Table/Table';
+import Dialog from 'ui/components/feedback/Dialog/Dialog';
+import JobInformation from 'ui/components/data-display/JobInformation/JobInformation';
+import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 
 // import { Component } from "@styles/pages/oportunidades.styled";
 
@@ -121,6 +124,51 @@ const Oportunidades: React.FC = () => {
       ) : (
         <Typography align="center">Nenhuma oportunidade ainda</Typography>
       )}
+
+      <Dialog
+        isOpen={false}
+        onClose={() => {}}
+        title={"Se candidatar à diária"}
+        subtitle={"Tem certeza que deseja se candidatar à diária abaixo?"}
+      >
+        <div>
+          <JobInformation>
+            <>
+              <div>
+                Data: <strong>01/01/2022</strong>
+              </div>
+              <div>Endereço:</div>
+              <div>
+                <strong>Valor: R$ 140,00</strong>
+              </div>
+            </>
+          </JobInformation>
+        </div>
+
+        <UserInformation name="João Vitor" rating={3} picture={""} />
+        <Divider />
+        <Typography sx={{ p: 3, fontWeight: "medium", bgcolor: "grey.50" }}>
+          Últimas avaliações do cliente
+        </Typography>
+        <UserInformation 
+          name="João Vitor" 
+          rating={3} 
+          picture={""} 
+          isRating={true}
+        />
+        <Typography 
+          sx={{ py: 2 }}
+          variant={"subtitle2"}
+          color={"textSecondary"}
+        >
+          Ao se candidatar você ainda não é o(a) diarista escolhido(a) para
+          realizar o trabalho. Vamos analisar suas qualificações e a distância
+          para o local da diária. Caso você seja a pessoa selecionada, receberá 
+          um email avisando. Atente-se à sua caixa de entrada! 
+        </Typography>
+      </Dialog> 
+        
+      
     </Container>
   );
 };
