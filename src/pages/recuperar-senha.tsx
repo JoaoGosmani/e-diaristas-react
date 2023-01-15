a4038b62ac3f4f64f36139c3fd9c898e5b64916f
Joao Gosmani <joaogosmani@gmail.com>
2023-01-14 23:18:55 -0300

    Criação da tela para resetar a senha

--- src/pages/recuperar-senha.tsx
+++ src/pages/recuperar-senha.tsx
@@ -4,6 +4,7 @@ import { useRecuperarSenha } from 'data/hooks/pages/useRecuperarSenha';
 import { GetStaticProps } from 'next';
 import React from 'react';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import PasswordStrength from 'ui/components/feedback/PasswordStrength/PasswordStrength';
 import TextField from 'ui/components/inputs/TextField/TextField';
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -20,7 +21,15 @@ const RecuperarSenha: React.FC = () => {
     <Container>
       <PageTitle title={"Recuperação de senha"} />
       {router.query.token ? (
-        <>Confirmação da senha</>
+        <FormFieldsContainer>
+          <TextField label={"Digite seu e-mail"} type={"email"} fullWidth />
+          <TextField label={"Digite a nova senha"} type={"password"} fullWidth />
+          <TextField label={"Confirme a nova senha"} type={"password"} fullWidth />
+          <PasswordStrength password="" />
+          <LoginButton size={"large"} variant={"contained"} color={"secondary"}>
+            Redefinir senha
+          </LoginButton>
+        </FormFieldsContainer>
       ) : (
         <FormFieldsContainer>
           <TextField label={"Digite seu e-mail"} type={"email"} />
