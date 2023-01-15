43127a816d0f4ff53ce84e8f77a0b775f2adaa6d
Joao Gosmani <joaogosmani@gmail.com>
2023-01-14 23:32:44 -0300

    Requisição para iniciar a criação de uma nova senha

--- src/data/hooks/pages/useRecuperarSenha.ts
+++ src/data/hooks/pages/useRecuperarSenha.ts
@@ -1,9 +1,42 @@
+import { ExternalServiceContext } from "data/contexts/ExternalServiceContext";
+import { ApiServiceHateoas } from "data/services/ApiService";
 import { useRouter } from "next/router";
+import { useContext, useState } from "react";
 
 export function useRecuperarSenha() {
-    const router = useRouter();
+    const router = useRouter(),
+        [email, setEmail] = useState(''),
+        { externalServicesState } = useContext(ExternalServiceContext),
+        [mensagemSnack, setMensagemSnack] = useState('');
+
+    async function pedirTokenRecuperacao() {
+        if (email.length > 8) {
+            ApiServiceHateoas(
+                externalServicesState.externalService, 
+                "solicitar_alteracao_senha", 
+                async (request) => {
+                    try {
+                        await request({
+                            data: {
+                                email,
+                            },
+                        });
+                        setMensagemSnack(
+                            "Uma mensagem foi enviada ao seu e-mail para a recuperação da senha"
+                        );
+                    } catch (error) {}
+                }
+            );
+        } else {
+            setMensagemSnack("Digite um e-mail válido");
+        }
+    }
 
     return {
         router,
+        email,
+        setEmail,
+        mensagemSnack,
+        setMensagemSnack,
     }
 }
